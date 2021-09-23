66b7e97cb11b774633b1f68ab4d5a068c3f74a9e
JMCalvi <77418073+JMCalvi@users.noreply.github.com>
2021-09-22 21:16:43 -0300

    TPO Calvi
    TPO Codo a Codo de Calvi

---
+++ js/form.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('btn')
+let input = document.getElementById('task')
+
+input.addEventListener('input', () => {
+    let n = 0;
+    for (let i = 0; i < input.value.length; i++) {
+        if (input.value[i] == '@') {
+            n++;
+        }
+    }   if (n == 1) {
+            button.disabled = false;
+    }   else {
+            button.disabled = true;
+    }
+})
